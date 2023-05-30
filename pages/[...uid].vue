c158861f38deae74a1b9e63615a79647427a935c
lihbr <lihbr@users.noreply.github.com>
2023-05-30 17:04:01 +0200

    fix: async data key

--- pages/[...uid].vue
+++ pages/[...uid].vue
@@ -3,7 +3,7 @@ import { components } from '~/slices'
 
 const prismic = usePrismic()
 const route = useRoute()
-const { data: page } = useAsyncData('_uid', () =>
+const { data: page } = useAsyncData('[uid]', () =>
   prismic.client.getByUID('page', route.params.uid as string || 'home')
 )
 
