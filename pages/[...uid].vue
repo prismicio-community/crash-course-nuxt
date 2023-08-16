cd73db39152a0407ad518b06689b1196d591595f
Thomas Foskolos <akumanara@gmail.com>
2023-08-16 14:59:48 +0300

    Error handling on uid page

--- pages/[...uid].vue
+++ pages/[...uid].vue
@@ -3,9 +3,14 @@ import { components } from '~/slices'
 
 const prismic = usePrismic()
 const route = useRoute()
-const { data: page } = useAsyncData('[uid]', () =>
+const { data: page, error } = useAsyncData('[uid]', () =>
   prismic.client.getByUID('page', route.params.uid as string || 'home')
 )
+
+// Handle the error
+if (error.value) {
+  throw createError({ statusCode: 404, message: 'Page not found' });
+}
 
 useHead({
   title: prismic.asText(page.value?.data.title)
