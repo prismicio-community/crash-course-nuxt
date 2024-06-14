093addf548c5692f081bfe29d6b43a43d8d197d9
lihbr <lihbr@users.noreply.github.com>
2024-06-14 16:45:46 +0200

    feat: migrate to Nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@ import { repositoryName } from './slicemachine.config.json'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: true,
+  future: {
+    compatibilityVersion: 4
+  },
+
+  devtools: { enabled: true },
 
   app: {
     head: {
@@ -26,6 +30,7 @@ export default defineNuxtConfig({
   css: ['~/styles/global.css'],
 
   modules: [
+    '@nuxt/eslint',
     '@nuxtjs/prismic',
     '@nuxtjs/tailwindcss'
   ],
