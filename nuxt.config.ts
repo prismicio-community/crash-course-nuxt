b914dc2e75701d316926495358ef99ac28478dbf
lihbr <lihbr@users.noreply.github.com>
2024-09-17 15:49:35 +0200

    chore(deps): maintain dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { repositoryName } from './slicemachine.config.json'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: true,
+  devtools: { enabled: true },
 
   app: {
     head: {
